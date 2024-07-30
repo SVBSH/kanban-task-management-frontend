9759881b0bb46047d838cac368ddb6b3449f49b9
SVBSH <getenv@protonmail.com>
2024-07-31 00:36:50 +0200

    add state for empty task group

--- src/components/TaskGroup/TaskGroup.jsx
+++ src/components/TaskGroup/TaskGroup.jsx
@@ -1,19 +1,33 @@
 import styles from './taskGroup.module.css'
 import TaskList from '../TaskList/TaskList'
 const TaskGroup = (props) => {
-  return (
-    <div className={styles['task-group-container'] + ' bg-primary-1'}>
-      <ul className={styles['task-group-list']} role="list">
-        <li className={styles['task-group-item']}>
-          <h2 className="color-gray-3">TODO (4)</h2>
-          <TaskList />
-        </li>
+  const boardEmpty = true
 
-        {/* TODO: add new column */}
-        <li className={styles['task-group-item']} data-new-column="true">
-          <h2 className="color-gray-3">+New Column</h2>
-        </li>
-      </ul>
+  return (
+    <div
+      data-board-empty={boardEmpty}
+      className={styles['task-group-container'] + ' bg-primary-1'}
+    >
+      {!boardEmpty ? (
+        <ul className={styles['task-group-list']} role="list">
+          <li className={styles['task-group-item']}>
+            <h2 className="color-gray-3">TODO (4)</h2>
+            <TaskList />
+          </li>
+
+          {/* TODO: add new column */}
+          <li className={styles['task-group-item']} data-new-column="true">
+            <h2 className="color-gray-3">+New Column</h2>
+          </li>
+        </ul>
+      ) : (
+        <>
+          <p className="color-gray-3 fw-500">
+            This board is empty. Create a new column to get started.
+          </p>
+          <button className="">+ Add New Column</button>
+        </>
+      )}
     </div>
   )
 }
